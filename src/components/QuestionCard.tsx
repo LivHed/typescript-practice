63a6faad7094e1b995a5c7ad3a1bfb5347ed37e1
LivHed <livia.hedin@gmail.com>
2020-11-06 20:11:36 +0100

    Add first structure to the question card component, type the props and add them to the function

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const QuestionCard = () => <div>Question Card</div>;
+// because of typescript I have to type the props here, (can be called something else than Props too)
+type Props = {
+  question: string;
+  answer: string[]; // meaning an array of strings
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+// This says that from React we have something called FC, which is the types from React itself, 
+// it will tell typescript that this is a functional component. And inside I specify that I want to use the Props created above.
+// structure out/specify the props inside the brackets, to be able to use them inside the component.
+const QuestionCard: React.FC<Props> = ({
+  question, 
+  answer, 
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (  
+<div>Question Card</div>
+);
 
 export default QuestionCard;
