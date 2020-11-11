0f88df6863a6bc2253ec49fbe73b6e97d0fb6dec
LivHed <livia.hedin@gmail.com>
2020-11-11 18:31:09 +0100

    Set initial states for the app and add JSX attributes to QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Components
 import QuestionCard from './components/QuestionCard';
 
+// API keep here atm: https://opentdb.com/api.php?amount=10&type=multiple
+
 const App = () => {
+
+  const TOTAL_QUESTIONS = 10;
+
+  // The initial states for the app.
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  //the state for which questionnumber the user is currently on.
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  // to keep track on when the game is finnished.
+  const [gameOver, setGameOver] = useState(true);
 
   // The function that will be fired off when the quiz is started. The API call will be made from this function.
   const startTrivia = async () => {
@@ -22,12 +36,19 @@ const App = () => {
   return (
       <div className="App">
         <h1>React Quiz</h1>
-        <button className="start" onclick={startTrivia}>
+        <button className="start" onClick={startTrivia}>
           Start
         </button>
         <p className="score">Score:</p>
         <p>Loading Questions..</p>
-        <QuestionCard />
+        <QuestionCard 
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined } // check if there is some answers in the userAnsers state, and if there is we can grab the correct answer by specifying the number. : Otherwise we just give it undefined. 
+          callback={checkAnswer}
+        />
         <button className="next" onClick={nextQuestion}>
           Next Question
         </button>
